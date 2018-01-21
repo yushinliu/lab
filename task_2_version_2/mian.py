502e611adde7abe996678cfb913d9790bf0605c8
Mata-wqq <Mata.wqq.cn@gmail.com>
2018-01-21 17:29:58 +0100

    weiqianqian

--- task_2_version_2/mian.py
+++ task_2_version_2/mian.py
@@ -1,7 +1,8 @@
 import package.py
 import data_import.py
 
-audio_path = "D:\\LAB\\workspace\\lab\\patRecDat\\forStudents\\timit\\test"
+#audio_path = "D:\\LAB\\workspace\\lab\\patRecDat\\forStudents\\timit\\test"
+audio_path = "/Users/Mata/Documents/2017/学习/ws2017:18/PUL/forStudents/timit/test"
 
 dataset=data_import(audio_path)  #samples is a dictionary of 172 persons
 
