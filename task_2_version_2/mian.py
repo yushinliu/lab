d4af88d4a4e9212ed5ae6aea395e5dc9e934b407
yushinliu <yuxinliu_oliver@outlook.com>
2018-01-21 17:27:03 +0100

    task2_merge

--- task_2_version_2/mian.py
+++ task_2_version_2/mian.py
@@ -1,5 +1,15 @@
 import package.py
+import data_import.py
 
 audio_path = "D:\\LAB\\workspace\\lab\\patRecDat\\forStudents\\timit\\test"
 
-frames=
+dataset=data_import(audio_path)  #samples is a dictionary of 172 persons
+
+for name in dataset.keys():
+	print(name)
+    
+
+	#frames=frame_func(samples)  #frames is a list with length of 
+
+	#window_frames=window_func(frames)
+
---
+++ task_2_version_2/mian.py.bak
@@ -0,0 +1,5 @@
+import package.py
+
+audio_path = "D:\\LAB\\workspace\\lab\\patRecDat\\forStudents\\timit\\test"
+
+frames=
