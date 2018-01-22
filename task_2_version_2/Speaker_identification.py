24e6cea181538b46051045bd9fd9e4853d688666
yushinliu <yuxinliu_oliver@outlook.com>
2018-01-22 22:04:36 +0100

    task2_modelbuild

---
+++ task_2_version_2/Speaker_identification.py
@@ -0,0 +1,14 @@
+import numpy as np
+from Speaker_model import naive_G_U
+
+#math
+import math
+
+K_value=49
+
+def Speaker_identification(b_test,new_mu,new_cov,new_weight,T_value):
+	 #caculate the concatenated probability
+	 test_pdf=naive_G_U(b_test,new_mu,new_cov,new_weight,T_value)
+     #print(test_pdf)
+     scores=np.sum(np.log(test_pdf))
+     return scores
