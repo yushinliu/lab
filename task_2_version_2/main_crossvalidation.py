f492e1510e3eff436a5a12bb6c9eeaab0ad92975
Mata-wqq <Mata.wqq.cn@gmail.com>
2018-01-22 23:19:55 +0100

    2018-weiqianqian

--- task_2_version_2/main_crossvalidation.py
+++ task_2_version_2/main_crossvalidation.py
@@ -4,6 +4,7 @@ from window_func import window_func
 from mel_func import mel_func
 from DCT_func import DCT_func
 from process_bar import ShowProcess
+from Speaker_model import Speaker_model
 
 #default
 
@@ -11,73 +12,78 @@ import numpy as np
 #loadmat
 from scipy.io import loadmat,savemat
 
-t_feed=10 #feed time
-t_frame=20 #frame time
-sample_rate=16000
-fs=sample_rate/1000 #sample_rate of each ms
-L_value=np.int(fs*t_frame)
-NFFT=512
-nfilt=22
-
 #audio_path = "D:\\LAB\\workspace\\lab\\patRecDat\\forStudents\\timit\\test"
 audio_path = "/Users/Mata/Documents/2017/学习/ws2017:18/PUL/forStudents/timit/test"
 
+#ubm_dataset=loadmat("/Users/Mata/Documents/2017/学习/ws2017:18/PUL/forStudents/ubm/UBM_GMMNaive_MFCC_Spectrum0to8000Hz.mat",mat_dtype=True)
+ubm_dataset=loadmat("C:\\Users\\hasee\\workspace\\workspace\\lab\\patRecDat\\forStudents\\ubm\\UBM_GMMNaive_MFCC_Spectrum0to8000Hz.mat",mat_dtype=True)
+ubm_means=ubm_dataset['means']
+ubm_var = ubm_dataset['var']
+ubm_weights = ubm_dataset['weights'].ravel()
+ubm_var_set=[]
+K_value=49
+gamma_UBM=0.001
+
+#transfer variance of UBM to cov
+for k in range(K_value):
+    ubm_var_set.append(np.diag(ubm_var[k]))
+ubm_var_set=np.array(ubm_var_set)
+ubm_var=ubm_var_set
+
 dataset=data_import(audio_path)  #samples is a dictionary of 172 persons
 
-feature_all_set={}
+features=loadmat("/Users/Mata/Documents/lab/features.mat")
 score_set=[]
 correct_num=0
 false_num=0
-#whole_set=[]
-
-test_file_set=[]
 name_set=[]
-train_file_set=[]
-features=[]
+#modul_set={}
 
-for name in dataset.keys():
+for name in features.keys():
     feature_all_set.setdefault(name)
-    test_file_set.setdefault(name)
-    train_file_set.setdefault(name)
     name_set.append(name)
+
+
 
 print("crossvalidation start")
 for cross_num in range 10:
-    
-    for name in dataset.keys():
-        
-        whole_set=dataset.get(name,'no such file').copy()
+    name_max=0
+    scores_max=0
+    for name in features.keys():
+        ubm_value_set=[]
+        whole_set=features.get(name,'no such file').copy()
+        whole_set=whole_set.reshape(15,-1)
         
         test_file=whole_set[cross_num]
-        test_file_set[name].append(test_file)
+        #test_file_set[name].append(test_file)
         #name_set.append(name)
         train_set=whole_set.copy()
         train_set.remove(test_file)
         train_set=np.concatenate(train_set)
-        train_file_set[name].append(train_set)
-
-        frames=frame_func(samples)  #frames is a list with length of 320*frames
-                
-        window_frames=window_func(frames) #using hanning window
-                
-        freq_frames=mel_func(window_frames)#
-                
-        features=DCT_func(freq_frames)# features: 15*frames
-                
-        features=features.ravel() # need to be reshape when import again
+        #train_file_set[name].append(train_set)
         
-        #features_set.append(features)
-                
-        #feature_all_set.setdefault(name)
-
-        feature_all_set[name].append(features_set)
-
-    for name_2,test_file in enumerate(test_file_set):
-        test_set=np.array(test_set)
-        K_value=ubm_var.shape[0]
-
-
-
+        test_set=np.array(test_file)
+        K_value=ubm_var.shape[0] #number of naive models
+        T_value=test_set.shape[1]
+        eva_set=[]
+        
+        for name_2 in features.keys():
+            
+        
+        #5.1 naive GMM-UBM
+        ubm_value_set=naive_G_U(train_set,ubm_means,ubm_var,ubm_weights,T_value)
+        #5.2 Speaker adaption
+        new_mu,new_cov,new_weight=Speaker_model(train_set,ubm_weights,ubm_means,ubm_var,ubm_value_set,T_value,gamma_UBM)
+        scores=identification(test_file,new_mu,new_cov,new_weight,T_value)
+        if scores>scores_max:
+           name_max = name
+    
+        
+        
+            if name_set[index_2] == test_name:
+        correct_num +=1
+else:
+    false_num +=1
 
 
 
